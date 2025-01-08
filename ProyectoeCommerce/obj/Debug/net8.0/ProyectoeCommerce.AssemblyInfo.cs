381157caf0b253dc47784af917fd8d905458b846
Lucía Godoy <luguada.g16@gmail.com>
2025-01-08 19:40:27 -0300

    Implementación de JWT

--- ProyectoeCommerce/obj/Debug/net8.0/ProyectoeCommerce.AssemblyInfo.cs
+++ ProyectoeCommerce/obj/Debug/net8.0/ProyectoeCommerce.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("eabd917a-ca29-4bbc-b4a8-18f4fc6e0c25")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoeCommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4aa74f04f0f66fa1ee430bdb626346f442edbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04493c946bd1f3d88d6160cf975d597e6ad1ae7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoeCommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoeCommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
